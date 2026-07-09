2301fe017670d878e8fceb37c49350a9261392e4
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-07-09 15:47:59 +1000

    Updated the default config in install.py

--- install.py
+++ install.py
@@ -17,7 +17,7 @@ from weecfg.extension import ExtensionInstaller
 
 VERSION = "2.0.6"
 
-MQTTPUBLISH_CONFIG = """
+CONFIG = """
 [MQTTPublishV2]
     # Whether the service is enabled or not.
     # Valid values: true or false
@@ -52,7 +52,7 @@ MQTTPUBLISH_CONFIG = """
     # The MQTT protocol to use
     # Valid values: MQTTv31, MQTTv311, MQTTv5
     # Default is MQTTv311,
-    protocol = MQTTv311
+    protocol = MQTTv5
 
     # username for broker authentication.
     # Default is None.
@@ -70,11 +70,11 @@ MQTTPUBLISH_CONFIG = """
         enable = false
 
         # Path to the Certificate Authority certificate files that are to be treated as trusted by this client.
-        ca_certs =
+        ca_certs = /etc/ssl/certs/ca-certificates.crt
 
         # The PEM encoded client certificate and private keys.
         # Default is None
-        certfile = None
+        # certfile = None
 
         # The certificate requirements that the client imposes on the broker.
         # Valid values: none, optional, required
@@ -83,11 +83,11 @@ MQTTPUBLISH_CONFIG = """
 
         # The encryption ciphers that are allowable for this connection. Specify None to use the defaults
         # Default is None.
-        ciphers = None
+        # ciphers = None
 
         # The private keys.
         # Default is None
-        keyfile = None
+        # keyfile = None
 
         # The version of the SSL/TLS protocol to be used.
         # Valid values: sslv2, sslv23, sslv3, tls, tlsv1, tlsv11, tlsv12.
@@ -119,7 +119,7 @@ MQTTPUBLISH_CONFIG = """
         retain = true
 
     [[topics]]
-        [[[REPLACE_ME]]]
+        [[[website/loop]]]
             # Controls if the topic is published.
             # Default is true.
             publish = false
@@ -131,26 +131,97 @@ MQTTPUBLISH_CONFIG = """
 
             # The binding, loop or archive.
             # Default is 'archive, loop'.
-            binding = archive, loop
+            binding = loop
 
             # The QOS level to publish to.
             # Default is 0
-            qos = 0
+            qos = 1
 
             # The MQTT retain flag.
             # The default is False.
-            retain = False
+            retain = true
 
             # The unit system for data published to this topic.
             # The default is US.
             unit_system = US
 
+            # True if all fields should not be published.
+            # Useful if you only want select fields to be published
+            # Valid values: True, False.
+            # Default is  False
+            ignore = true
+
             [[[[fields]]]]
-                [[[[[REPLACE_ME]]]]]
+                [[[[[outTemp]]]]]
                     # True if the field should not be published.
                     # Valid values: True, False.
                     # Default is  False
                     ignore = true
+
+                    # The WeeWX name of the data to be published.
+                    # Default is the config section name.
+                    # name =
+
+                    # The WeeWX unit to convert the data being published to.
+                    # Default is None.
+                    # unit =
+
+                    # Controls if data with a value of 'None' should be published.
+                    # The default is false.
+                    publish_none_value = true
+
+                    # Controls if the WeewX unit label should be append to the data being published.
+                    # The default is true.
+                    append_unit_label = false
+
+                    # The data type conversion to apply to the data being published.
+                    # The default is not to convert.
+                    # conversion_type = string
+
+                    # The formatting to apply to the data being published.
+                    # The default is '%s'.
+                    # format_string = %s
+
+        [[[website/archive]]]
+            # Controls if the topic is published.
+            # Default is true.
+            publish = false
+
+            # The format of the MQTT payload.
+            # Currently support: individual, json, keyword
+            # The default is 'json'
+            type = json
+
+            # The binding, loop or archive.
+            # Default is 'archive, loop'.
+            binding = archive
+
+            # The QOS level to publish to.
+            # Default is 0
+            qos = 1
+
+            # The MQTT retain flag.
+            # The default is False.
+            retain = true
+
+            # The unit system for data published to this topic.
+            # The default is US.
+            unit_system = US
+
+            # True if all fields should not be published.
+            # Useful if you only want select fields to be published
+            # Default is  False
+            ignore = true
+
+            [[[[fields]]]]
+                [[[[[dateTime]]]]]
+                    ignore = false
+
+                [[[[[outTemp]]]]]
+                    # True if the field should not be published.
+                    # Valid values: True, False.
+                    # Default is  False
+                    ignore = false
 
                     # The WeeWX name of the data to be published.
                     # Default is the config section name.
@@ -176,26 +247,56 @@ MQTTPUBLISH_CONFIG = """
                     # The default is '%s'.
                     format_string = %s
 
-            # The aggregations to perform
-            [[[[aggregates]]]]
-                # The name of the observation in the MQTT payload.
-                # This can be any name. For example: rainSumDay, outTempMinHour, etc
-                [[[[[REPLACE_ME]]]]]
-                    # Turn aggregates on and off.
-                    # Default is true.
-                    enable = false
+        [[[website/stats]]]
+            # Controls if the topic is published.
+            # Default is true.
+            publish = false
 
-                    # The WeeWX observation to aggregate, rain, outTemp, etc,
-                    observation =
+            # The format of the MQTT payload.
+            # Currently support: individual, json, keyword
+            # The default is 'json'
+            type = json
 
-                    # The type of aggregation to perform.
-                    # See, https://www.weewx.com/docs/customizing.htm#aggregation_types
-                    aggregation =
+            # The binding, loop or archive.
+            # Default is 'archive, loop'.
+            binding = archive
 
-                    # The time period over which the aggregation shoulf occurr.
-                    # Valid values: hour, day, week, month, year, yesterday, last24hours, last7days, last31days, last366days
-                    period =
+            # The QOS level to publish to.
+            # Default is 0
+            qos = 1
 
+            # The MQTT retain flag.
+            # The default is False.
+            retain = true
+
+            # The unit system for data published to this topic.
+            # The default is US.
+            unit_system = US
+
+            # True if all fields should not be published.
+            # Useful if you only want select fields to be published
+            # Default is  False
+            ignore = true
+
+            # Set the following to true to auto-populate *_outTemp_min etc
+            # Options: min, max, both or sum
+            minmax = both
+
+            # Set the following to true to auto-populate day_outTemp_*, yesterday_* etc
+            day = true
+            yesterday = true
+            month = true
+            last_month = true
+            year = true
+            last_year = true
+            alltime = true
+
+            [[[[fields]]]]
+                [[[[[dateTime]]]]]
+                    ignore = false
+
+                [[[[[outTemp]]]]]
+                    ignore = false
 """
 
 def loader():
@@ -207,6 +308,8 @@ class MQTTPublishInstaller(ExtensionInstaller):
     """ The extension installer. """
     def __init__(self):
 
+        config_dict = configobj.ConfigObj(StringIO(CONFIG))
+
         install_dict = {
             'version': VERSION,
             'name': 'MQTTPublishV2',
@@ -214,12 +317,9 @@ class MQTTPublishInstaller(ExtensionInstaller):
             'description': ' Publish WeeWX data to a MQTT broker.',
             'author': 'John Smith',
             'author_email': "deltafoxtrot256+MQTTPublishV2@gmail.com",
-            'files': [('bin/user', ['bin/user/MQTTPublishV2.py'])]
+            'files': [('bin/user', ['bin/user/MQTTPublishV2.py'])],
+            'config': config_dict,
+            'restful_services': 'user.MQTTPublishV2.PublishWeeWX',
         }
-
-        mqttpublish_dict = configobj.ConfigObj(StringIO(MQTTPUBLISH_CONFIG))
-        install_dict['config'] = mqttpublish_dict
-        # ToDo: Better service group?
-        install_dict['restful_services'] = 'user.MQTTPublishV2.PublishWeeWX'
 
         super().__init__(install_dict)
